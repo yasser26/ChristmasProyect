c1055f7620db49d26914b8a3a9bebb2f27304678
yasser26 <yasserwagon@hotmail.com>
2017-12-13 16:53:48 -0600

    leer datasets desde un archivo

--- leerDataSet.py
+++ leerDataSet.py
@@ -7,11 +7,25 @@ from sklearn.cluster import KMeans
 
 
 
+def leerDataSet():
+    data0 = pd.read_csv('iris_dataset.csv');
+    tags = np.asarray(data0.columns.values);
+    data = np.asarray(data0);
+    #print data;
+
+    ff = data[:data.shape[0],-1];
+    ff2 = ff.reshape(data.shape[0], 1);
+    #print ff2;
+    #print data.shape;
+
+    iris_data = pd.DataFrame(data[:data.shape[0],:data.shape[1]-1], columns=tags[:data.shape[1]-1]);
+    iris_label = pd.DataFrame(ff2, columns=[tags[data.shape[1]-1]]);
+    print iris_data;
+    print iris_label;
+
+
 def main():
-    data = pd.read_csv('iris_dataset.csv')
-    print data
-
-
+    leerDataSet()
 
 
 if __name__ == '__main__':
